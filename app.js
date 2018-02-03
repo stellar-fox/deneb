43636f3ec58bf3ddae1bbea7fb037e2249a28907
syntaxval <syntaxval@users.noreply.github.com>
2018-02-03 16:01:53 +0100

    Auto sign all commits

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! Signing commits!'))
 
 app.listen(4001, () => console.log('App listenin on 4001.'))
