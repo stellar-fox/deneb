bc37a011b4f4c3df0f99addded25601334a1f2de
syntaxval <syntaxval@users.noreply.github.com>
2018-02-04 14:43:54 +0100

    Allow CORS

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const GETAPI = require('./api/v1/get.js')
 const express = require('express')
 const app = express()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 /*
  *********************
