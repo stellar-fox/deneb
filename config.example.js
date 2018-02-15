0af9761963a28a7e021e12670bf4ffd15db1f567
syntaxval <syntaxval@users.noreply.github.com>
2018-02-15 20:35:10 +0100

    Update ESLint directives and cleanup

--- config.example.js
+++ config.example.js
@@ -1,4 +1,4 @@
 exports.attributes = {
-  connectionStr: 'postgres://user:password@host:port/database',
-  apiKey: '03565689-061b-4b7e-8ae3-e6ac69837958',
+    connectionStr: "postgres://user:password@host:port/database",
+    apiKey: "03565689-061b-4b7e-8ae3-e6ac69837958",
 }
