af1a17291d7608f61b1c7983500a42baa6da55f5
syntaxval <syntaxval@users.noreply.github.com>
2018-07-15 17:31:05 +0200

    Distribute custom token upon account fund

--- api/v2/account/post.js
+++ api/v2/account/post.js
@@ -1,6 +1,40 @@
 const helpers = require("../../helpers")
 const stripe = require("stripe")(helpers.config.stripe.apiKey)
+const StellarSdk = require("stellar-sdk")
+const BigNumber = require("bignumber.js")
 
+
+
+
+// ...
+const sendAsset = (destinationId, amount) => {
+
+    StellarSdk.Network.useTestNetwork()
+    const server = new StellarSdk.Server(helpers.config.stellar.horizon)
+    var sourceKeys = StellarSdk.Keypair.fromSecret(helpers.config.stellar.distributionSecret)
+
+    server.loadAccount(sourceKeys.publicKey())
+        .then((sourceAccount) => {
+            const transaction = new StellarSdk.TransactionBuilder(sourceAccount)
+                .addOperation(StellarSdk.Operation.payment({
+                    destination: destinationId,
+                    asset: new StellarSdk.Asset(
+                        helpers.config.stellar.assetCode,
+                        helpers.config.stellar.issuingPublic
+                    ),
+                    amount,
+                }))
+                // Use the memo to indicate the customer this payment is intended for.
+                .addMemo(StellarSdk.Memo.text(helpers.config.stellar.distMemo))
+                .build()
+            transaction.sign(sourceKeys)
+            return server.submitTransaction(transaction)
+        })
+        .catch(function (error) {
+            // eslint-disable-next-line no-console
+            console.log(error.response.data.extras.result_codes)
+        })
+}
 
 
 
@@ -14,11 +48,16 @@ const fund = async (req, res, _next) => {
             source: req.body.charge.token,
         })
 
+
+        await sendAsset(req.body.charge.publicKey,
+            (new BigNumber(req.body.charge.amount))
+                .dividedBy(100).toString()
+        )
+
+
         return res.status(200).json({ status, })
 
     } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log(error.message)
         return res.status(500).json({ error: error.message, })
     }
 }
