65af7704a2ff261c70c6381c2edcb662dd986be1
syntaxval <syntaxval@users.noreply.github.com>
2018-07-16 17:20:15 +0200

    Record failed transaction in firestore

--- api/v2/account/post.js
+++ api/v2/account/post.js
@@ -4,18 +4,19 @@ const StellarSdk = require("stellar-sdk")
 const BigNumber = require("bignumber.js")
 
 
-
-
 // ...
 const sendAsset = (destinationId, amount) => {
 
     StellarSdk.Network.useTestNetwork()
     const server = new StellarSdk.Server(helpers.config.stellar.horizon)
-    var sourceKeys = StellarSdk.Keypair.fromSecret(helpers.config.stellar.distributionSecret)
+    const sourceKeys = StellarSdk.Keypair.fromSecret(
+        helpers.config.stellar.distributionSecret
+    )
+    let transaction = null
 
-    server.loadAccount(sourceKeys.publicKey())
+    return server.loadAccount(sourceKeys.publicKey())
         .then((sourceAccount) => {
-            const transaction = new StellarSdk.TransactionBuilder(sourceAccount)
+            transaction = new StellarSdk.TransactionBuilder(sourceAccount)
                 .addOperation(StellarSdk.Operation.payment({
                     destination: destinationId,
                     asset: new StellarSdk.Asset(
@@ -24,13 +25,19 @@ const sendAsset = (destinationId, amount) => {
                     ),
                     amount,
                 }))
-                // Use the memo to indicate the customer this payment is intended for.
                 .addMemo(StellarSdk.Memo.text(helpers.config.stellar.distMemo))
                 .build()
             transaction.sign(sourceKeys)
             return server.submitTransaction(transaction)
         })
         .catch(function (error) {
+            /**
+             * Store transaction envelope that could not be submitted
+             */
+            helpers.firestore.collection("transactions").add({
+                xdrBody: transaction.toEnvelope().toXDR().toString("base64"),
+                submitted: false,
+            })
             // eslint-disable-next-line no-console
             console.log(error.response.data.extras.result_codes)
         })
@@ -48,12 +55,10 @@ const fund = async (req, res, _next) => {
             source: req.body.charge.token,
         })
 
-
         await sendAsset(req.body.charge.publicKey,
             (new BigNumber(req.body.charge.amount))
                 .dividedBy(100).toString()
         )
-
 
         return res.status(200).json({ status, })
 
