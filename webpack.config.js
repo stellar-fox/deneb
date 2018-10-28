404318e2ae69a29c0758327ec91263f6d4bb88d5
drmats <drmats@users.noreply.github.com>
2018-10-28 20:45:46 +0100

    Config: 'npm run build' and 'npm run production' possible.

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+"use strict"
+
+
+
+
+// ...
+const
+    fs = require("fs"),
+    path = require("path"),
+    webpack = require("webpack"),
+    MinifyPlugin = require("babel-minify-webpack-plugin"),
+    appDirectory = fs.realpathSync(process.cwd()),
+    nodeExternals = require("webpack-node-externals")
+
+
+
+
+// ...
+module.exports = {
+
+    mode: "production",
+
+
+    target: "node",
+
+
+    externals: [nodeExternals()],
+
+
+    entry: {
+        "deneb": path.resolve(
+            appDirectory, "src/index.js"
+        ),
+    },
+
+
+    output: {
+        filename: "[name].js",
+        chunkFilename: "[name].chunk.js",
+        path: path.resolve(__dirname, "./dist"),
+        globalObject: "this",
+    },
+
+
+    optimization: {
+        minimize: true,
+        mergeDuplicateChunks: true,
+        sideEffects: true,
+        providedExports: true,
+        concatenateModules: true,
+        occurrenceOrder: true,
+        removeEmptyChunks: true,
+        removeAvailableModules: true,
+        minimizer: [
+            new MinifyPlugin({}, {
+                comments: false,
+            }),
+        ],
+    },
+
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+                sideEffects: false,
+            },
+        ],
+    },
+
+
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env.BABEL_ENV": JSON.stringify("production"),
+        }),
+    ],
+
+}
