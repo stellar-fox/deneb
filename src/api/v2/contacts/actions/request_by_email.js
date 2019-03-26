a2360460100a08a47b38acdca8a362510e014f14
syntaxval <syntaxval@users.noreply.github.com>
2019-03-26 14:35:04 +0100

    Fix request by email flow and Mailchimp update. Format imports.

--- src/api/v2/contacts/actions/request_by_email.js
+++ src/api/v2/contacts/actions/request_by_email.js
@@ -15,10 +15,8 @@ import md5 from "blueimp-md5"
 import {
     mailchimp as mailchimpConfig,
 } from "../../../../config/configuration.json"
-
 import { contactStatusCodes } from "../../../../lib/helpers"
 import { sql } from "../../../../lib/utils"
-
 import getContactIdSQL from "./get_contact_id.sql"
 import getUserIdSQL from "./get_user_id.sql"
 import insertContactSQL from "./insert_contact.sql"
@@ -45,6 +43,19 @@ export default function requestByEmail (sqlDatabase) {
             { email: req.body.email }
         )
 
+        /** ===================================================================
+         *   Scenarios:
+         *
+         *      1. The requestee already exists on the platform.
+         *          1a. The requestee has had contact connection to the requestor.
+         *          1b. The requestee never had contact connection to the requestor.
+         *
+         *      2. The requestee does not exist on the platform.
+         *
+         ====================================================================*/
+
+
+        // Scenario 1.
         if (registeredUser) {
             const contact_id = await sqlDatabase.oneOrNone(
                 sql(__dirname, getContactIdSQL),
@@ -56,7 +67,9 @@ export default function requestByEmail (sqlDatabase) {
                 (e) => e && e.contact_id
             )
 
+            // Scenario 1a.
             if (contact_id) {
+
                 try {
                     await sqlDatabase.tx((t) => {
                         return t.batch([
@@ -83,22 +96,14 @@ export default function requestByEmail (sqlDatabase) {
                     return next(error.message)
                 }
 
+            // Scenario 1b.
             } else {
+
                 try {
                     await sqlDatabase.tx((t) => {
+
                         return t.batch([
-                            t.none(
-                                sql(__dirname, insertContactSQL),
-                                {
-                                    contact_id: registeredUser.id,
-                                    requested_by: req.body.user_id,
-                                    status: contactStatusCodes.REQUESTED,
-                                    created_at: now,
-                                    updated_at: now,
-                                    request_str: "",
-                                }
-                            ),
-                            // reciprocal - along with email of requestee
+
                             t.none(
                                 sql(__dirname, insertContactSQL),
                                 {
@@ -110,15 +115,31 @@ export default function requestByEmail (sqlDatabase) {
                                     request_str: req.body.email,
                                 }
                             ),
+
+
+                            t.none(
+                                sql(__dirname, insertContactSQL),
+                                {
+                                    contact_id: registeredUser.id,
+                                    requested_by: req.body.user_id,
+                                    status: contactStatusCodes.REQUESTED,
+                                    created_at: now,
+                                    updated_at: now,
+                                    request_str: "",
+                                }
+                            ),
                         ])
                     })
-                } catch (_error) {
+
+                } catch (error) {
                     res.status(409).send()
                     next()
                 }
             }
         }
 
+
+        // Scenario 1 or 2 - try to subscribe the email along with referrer.
         try {
             const client = axios.create({
                 auth: {
@@ -136,30 +157,36 @@ export default function requestByEmail (sqlDatabase) {
                     md5(req.body.email.toLowerCase())
                 }`)
 
-                // email is already on the subscription list
-                if (subscriber.data.status === "subscribed") {
-                    res.status(409).send()
-                    next()
+                if (!subscriber) {
+                    await client.post(`${mailchimpConfig.api}lists/${
+                        mailchimpConfig.lists.searchByEmail}/members/`, {
+                        email_address: req.body.email,
+                        status: "subscribed",
+                        merge_fields: {
+                            REFERRER: req.body.referrer.email,
+                            REFERRERFN: req.body.referrer.first_name,
+                            REFERRERLN: req.body.referrer.last_name,
+                        },
+                    })
+                } else {
+                    // update REFERRER only if different from the last one
+                    if (subscriber.data.merge_fields.REFERRER !== req.body.referrer.email) {
+                        await client.patch(`${mailchimpConfig.api}lists/${
+                            mailchimpConfig.lists.searchByEmail}/members/${subscriber.data.id}`, {
+                            merge_fields: {
+                                REFERRER: req.body.referrer.email,
+                                REFERRERFN: req.body.referrer.first_name,
+                                REFERRERLN: req.body.referrer.last_name,
+                            },
+                        })
+                    }
                 }
 
-                // TODO: perhaps detect here different subscription states
-                // and return proper code along with message.
-                res.status(409).send()
+                res.status(201).send()
                 next()
 
-            } catch (_error) {
-                // email is not yet on the subscription list
-                await client.post(`${mailchimpConfig.api}lists/${
-                    mailchimpConfig.lists.searchByEmail}/members/`, {
-                    email_address: req.body.email,
-                    status: "subscribed",
-                    merge_fields: {
-                        REFERRER: req.body.referrer.email,
-                        REFERRERFN: req.body.referrer.first_name,
-                        REFERRERLN: req.body.referrer.last_name,
-                    },
-                })
-                res.status(201).send()
+            } catch (error) {
+                res.status(409).send()
                 next()
             }
 
